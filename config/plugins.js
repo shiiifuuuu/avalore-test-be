7f5bd9b08924e2482814ec60d2d6be61721574ef
shiiifuuuu <mdshahadat.hossain7@gmail.com>
2023-08-24 22:23:52 +0600

    graphql plugin

---
+++ config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  //
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
